32cb4f67f98011ab65870614d419880aeee9001f
Artur Carvalho <arturbelico@gmail.com>
2022-07-27 21:23:14 +0200

    Add react layout

---
+++ src/layouts/PageLayout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function PageLayout({ children }) {
+  return (
+    <div>
+      Imagine a table of contents here
+      {children}
+    </div>
+  );
+}
